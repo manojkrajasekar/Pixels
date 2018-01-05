6dbc57fa5d98ba3a7f478c907c747940902abb09
manojkrajasekar <manoj.k.rajasekar@gmail.com>
2018-01-04 22:20:50 -0500

    Stored procedures for USER table

--- Database/Stored_Procedures/USER_TABLE.sql
+++ Database/Stored_Procedures/USER_TABLE.sql
@@ -2,23 +2,31 @@ USE photoapp;
 
 /* This is executed, when a new user is added. User details are stored in the USER table */
 DROP PROCEDURE IF EXISTS REGISTER_USER;
-DELIMITER //
+DELIMITER
+    //
 CREATE PROCEDURE REGISTER_USER
 (
-	 IN _MAIL_ID VARCHAR(254),
-	 IN _FIRST_NAME VARCHAR(50), 
-	 IN _LAST_NAME VARCHAR(50),
-	 OUT  _USER_ID INTEGER
+    IN _MAIL_ID VARCHAR(254),
+    IN _FIRST_NAME VARCHAR(50),
+    IN _LAST_NAME VARCHAR(50),
+    OUT _USER_ID INTEGER
 )
 BEGIN
-	INSERT INTO USER(MAIL_ID, FIRST_NAME, LAST_NAME) VALUES(_MAIL_ID, _FIRST_NAME, _LAST_NAME);
-	
-	SELECT LAST_INSERT_ID() 
-		INTO _USER_ID 
-			FROM USER U 
-	WHERE U.MAIL_ID = _MAIL_ID;
-END;
-//
+    INSERT INTO USER(MAIL_ID, FIRST_NAME, LAST_NAME)
+    VALUES
+    (
+        _MAIL_ID,
+        _FIRST_NAME,
+        _LAST_NAME
+    ) ;
+    SELECT
+        LAST_INSERT_ID()
+    INTO _USER_ID
+    FROM
+        USER U
+    WHERE
+        U.MAIL_ID = _MAIL_ID ;
+END ; //
 DELIMITER ;
 
 /* ------------------------------------------------------------------------------------------ */
@@ -36,8 +44,10 @@ BEGIN
 		U.FIRST_NAME, 
 		U.LAST_NAME,
 		U.NO_OF_POSTS
-			FROM USER U
-	WHERE U.USER_ID = _USER_ID;
+	FROM
+		USER U
+	WHERE 
+		U.USER_ID = _USER_ID;
 END;
 //
 DELIMITER ;
@@ -46,24 +56,25 @@ DELIMITER ;
 /* ------------------------------------------------------------------------------------------ */
 
 /* Updates the user's firstname or lastname, or both */
-DROP PROCEDURE IF EXISTS UPDATE_USER_DETAILS; 
-DELIMITER //
+DROP PROCEDURE IF EXISTS UPDATE_USER_DETAILS;
+DELIMITER
+    //
 CREATE PROCEDURE UPDATE_USER_DETAILS
 (
-	IN _USER_ID VARCHAR(254),
-	IN _FIRST_NAME VARCHAR(50), 
-	IN _LAST_NAME VARCHAR(50)
+    IN _USER_ID VARCHAR(254),
+    IN _FIRST_NAME VARCHAR(50),
+    IN _LAST_NAME VARCHAR(50)
 )
 BEGIN
-	UPDATE USER U
-		SET 
-			U.FIRST_NAME = _FIRST_NAME,
-			U.LAST_NAME = _LAST_NAME
-		WHERE U.USER_ID = _USER_ID;
-END;
-//
+    UPDATE
+        USER U
+    SET
+        U.FIRST_NAME = _FIRST_NAME,
+        U.LAST_NAME = _LAST_NAME
+    WHERE
+        U.USER_ID = _USER_ID ;
+END ; //
 DELIMITER ;
-
 
 /* ------------------------------------------------------------------------------------------ */
 
@@ -78,9 +89,13 @@ CREATE PROCEDURE TOGGLE_USER
 BEGIN
 	DECLARE ACTIVE BOOLEAN;
 	
-	SELECT IS_ACTIVE INTO ACTIVE
-		FROM USER U
-	WHERE U.USER_ID = _USER_ID;
+	SELECT 
+		IS_ACTIVE
+	INTO ACTIVE
+	FROM
+		USER U
+	WHERE
+		U.USER_ID = _USER_ID;
 	
 	IF ACTIVE = 0 THEN
 		UPDATE USER U
