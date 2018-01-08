02b20145e4dd36d52415165d634a7c8cfd60327e
manojkrajasekar <manoj.k.rajasekar@gmail.com>
2018-01-07 21:38:51 -0500

    functionality for comments table

--- Database/Stored_Procedures/comments.sql
+++ Database/Stored_Procedures/comments.sql
@@ -1,5 +1,5 @@
 /* This stored procedure when executed, saves the added comment in the comments table */
-/* What to do if ay one or all of these: user_id, post_id or comment_content is null  */
+/* What to do if any one or all of these: user_id, post_id or comment_content is null  */
 DROP PROCEDURE IF EXISTS add_comment;
 DELIMITER //
 CREATE PROCEDURE add_comment
@@ -10,7 +10,18 @@ CREATE PROCEDURE add_comment
 	OUT _comment_id INTEGER
 )
 BEGIN
-	INSERT INTO comments(user_id, post_id, comment_content) VALUES(_user_id, _post_id, _comment_content);
+	INSERT INTO comments
+	(
+		user_id,
+		post_id, 
+		comment_content
+	)
+	VALUES
+	(
+		_user_id,
+		_post_id, 
+		_comment_content
+	);
 	
 	SELECT last_insert_id() INTO _comment_id;
 END;
@@ -32,11 +43,11 @@ BEGIN
 		u.first_name AS 'Commented by',
 		c.comment_content AS 'Comment',
 		c.upload_time AS 'Time'
-			FROM users u
-			JOIN comments c ON c.user_id = u.user_id 
-			WHERE c.post_id = _post_id AND c.is_active = 1 
-			ORDER BY c.upload_time 
-			DESC LIMIT _limit ; 
+			FROM comments c 
+			JOIN users u ON u.user_id = c.user_id 
+			WHERE c.post_id = _post_id AND c.is_active = 1 AND u.is_active = 1 
+			ORDER BY c.upload_time DESC
+			LIMIT _limit ; 
 END;
 //
 DELIMITER ;
@@ -49,14 +60,14 @@ DELIMITER //
 CREATE PROCEDURE update_comment
 (
 	IN _comment_content VARCHAR(200), 
-	IN _comment_id INTEGER, 
-	IN _post_id INTEGER,
-	IN _user_id INTEGER
+	IN _comment_id INTEGER
 )
 BEGIN
 	UPDATE comments c
-		SET comment_content = _comment_content
-		WHERE c.comment_id = _comment_id AND c.post_id = _post_id;
+		SET
+			c.comment_content = _comment_content,
+			c.upload_time = current_timestamp +0
+		WHERE c.comment_id = _comment_id AND c.is_active = 1;
 END;
 //
 DELIMITER ;
@@ -68,14 +79,12 @@ DROP PROCEDURE IF EXISTS delete_comment;
 DELIMITER //
 CREATE PROCEDURE delete_comment
 (
-	IN _comment_id INTEGER, 
-	IN _user_id INTEGER,
-    IN _post_id INTEGER
+	IN _comment_id INTEGER 
 )
 BEGIN
-	UPDATE comments c
-		SET c.is_active = 0
-		WHERE c.comment_id = _comment_id AND c.post_id = _post_id;
+	UPDATE comments 
+		SET is_active = 0
+		WHERE comment_id = _comment_id AND c.is_active = 1;
 END;
 //
 DELIMITER ;
