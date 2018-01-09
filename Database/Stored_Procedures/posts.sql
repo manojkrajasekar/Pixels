ae7d4919b24f2f505422c2c97bba6654a0ec1e6e
manojkrajasekar <manoj.k.rajasekar@gmail.com>
2018-01-08 20:33:23 -0500

    functionality for postss table

--- Database/Stored_Procedures/posts.sql
+++ Database/Stored_Procedures/posts.sql
@@ -1,6 +1,9 @@
+USE photoapp;
+
+/* This stored procedure is executed, when a picture is posted and the details of that pic are stored in the database */
+/* TODO: Have to check whether the user is active ? */
+/* QUESTION: what to do if any one (or) all of the fields are NULL ? */
 DROP PROCEDURE IF EXISTS add_post;
-/* This stored procedure is executed, when a photo is posted and the details are stored in the database */
-/* Have to check whether the user is active, and all other fields must not be null */
 DELIMITER //
 CREATE PROCEDURE add_post
 (
@@ -12,12 +15,6 @@ CREATE PROCEDURE add_post
 	OUT _post_id INTEGER
 )
 BEGIN
-	DECLARE EXIT HANDLER FOR 1452
-	SELECT 'MySQL error 1452: Cannot add or update a child row: a foreign key constraint fails';
-	
-	DECLARE EXIT HANDLER FOR 1062
-	SELECT 'MySQL error 1062: Duplicate ket entry for primary key';
-	
 	INSERT INTO posts
 	(
 		user_id,
@@ -35,14 +32,14 @@ BEGIN
 		_next_topic
 	);
 	
-	SELECT last_insert_id() INTO _post_id;
+	SELECT LAST_INSERT_ID() INTO _post_id;
 END;
 //
 DELIMITER ;
 
 
-/* This stored procedure when executed, displays all the comments for a particular post */
-/* What to do if the topic_id is Null */
+/* Based on the given topic id, it displays all the posts */
+/* QUESTION: What to do if the topic_id is NULL ? */
 DROP PROCEDURE IF EXISTS get_posts_by_topic;
 DELIMITER //
 CREATE PROCEDURE get_posts_by_topic
@@ -51,29 +48,33 @@ CREATE PROCEDURE get_posts_by_topic
 )
 BEGIN
 	SELECT  
-		p.post_id AS 'POST ID',
-		p.url AS 'URL',
-		p.description AS 'Post Description',
-        u.first_name AS 'Posted by',
-        COUNT(v.post_id) AS 'Vote count',
-		COUNT(c.post_id) AS 'comments count'
+			p.post_id AS 'POST ID',
+			p.url AS 'URL',
+			p.description AS 'Post Description',
+			u.first_name AS 'Posted by',
+			COUNT(DISTINCT v.vote_id) AS 'Vote count',
+			COUNT(DISTINCT c.comment_id) AS 'comments count'
 		FROM posts p 
 		JOIN users u
-        	    ON p.user_id = u.user_id
+			ON p.user_id = u.user_id
+				AND p.topic_id = _topic_id 
+				AND u.is_active = 1
+				AND p.is_active = 1
 		LEFT JOIN votes v 
-                ON p.post_id = v.post_id AND v.is_active = 1
-		JOIN comments c 
-                ON p.post_id = c.post_id AND c.is_active = 1
-		WHERE p.topic_id = _topic_id AND u.is_active = 1
-        GROUP BY p.post_id;
+			ON p.post_id = v.post_id 
+				AND v.is_active = 1
+		LEFT JOIN comments c 
+			ON p.post_id = c.post_id
+				AND c.is_active = 1
+		GROUP BY p.post_id;
 END;
 //
 DELIMITER ;
 
 
 
-/* This stored procedure when executed, displays all the comments for a particular post */
-/* What to do if the user_id is Null */
+/* Based on the given user id, it displays all the posts of that particular user */
+/* QUESTION: What to do if the user_id is NULL ? */
 DROP PROCEDURE IF EXISTS get_posts_by_user;
 DELIMITER //
 CREATE PROCEDURE get_posts_by_user
@@ -81,25 +82,34 @@ CREATE PROCEDURE get_posts_by_user
 	IN _user_id INTEGER
 )
 BEGIN
-	SELECT  
-		p.post_id AS 'POST ID',
-		p.url AS 'URL',
-		p.description AS 'Post Description',
-        u.first_name AS 'Posted by',
-        COUNT(v.post_id) AS 'Vote count'
-        FROM posts p 
-        JOIN users u
-        	    ON p.user_id = u.user_id
+	SELECT 
+			p.post_id AS 'POST ID',
+			p.url AS 'URL',
+			p.description AS 'Post Description',
+			u.first_name AS 'Posted by',
+			COUNT(DISTINCT v.vote_id) AS 'Vote count',
+			COUNT(DISTINCT c.comment_id) AS 'comments count'
+		FROM posts p 
+		JOIN users u
+			ON p.user_id = u.user_id
+				AND p.user_id = _user_id
+				AND u.is_active = 1
+				AND p.is_active = 1
 		LEFT JOIN votes v 
-                ON p.post_id = v.post_id AND v.is_active = 1
-        WHERE p.user_id = _user_id
-        GROUP BY p.user_id;
+			ON p.post_id = v.post_id 
+				AND v.is_active = 1
+		LEFt JOIN comments c 
+			ON p.post_id = c.post_id 
+				AND c.is_active = 1
+		GROUP BY p.post_id;
 END;
 //
 DELIMITER ;
 
 
-
+/* Fetches the voter information(First name of the voter) of a particular post, 
+ by passing post_id as the input parameter */
+ /* QUESTION: What to do if the post_id is NULL ? */
 DROP PROCEDURE IF EXISTS get_voter_info_by_post;
 DELIMITER //
 CREATE PROCEDURE get_voter_info_by_post
@@ -108,11 +118,13 @@ CREATE PROCEDURE get_voter_info_by_post
 )
 BEGIN
 	SELECT  
-		u.first_name AS 'Voted by'
+			u.first_name AS 'Voted by'
         FROM votes v 
-        LEFT JOIN users u
-		ON v.user_id = u.user_id AND u.is_active = 1 
-		WHERE v.post_id = _post_id AND v.is_active = 1 
+        JOIN users u
+			ON v.user_id = u.user_id
+				AND v.post_id = _post_id 
+				AND u.is_active = 1 
+				AND v.is_active = 1 
         GROUP BY v.vote_id;
 END;
 //
@@ -122,7 +134,7 @@ DELIMITER ;
 
 
 /* This stored procedure when executed, the description for the post is updated */
-/* What to do if any one or all of these: user_id, post_id or description is null  */
+/* QUESTION: What to do if any one (or) all of the input parameters are null  */
 DROP PROCEDURE IF EXISTS update_post;
 DELIMITER //
 CREATE PROCEDURE update_post
@@ -131,18 +143,18 @@ CREATE PROCEDURE update_post
 	IN _description VARCHAR(100)
 )
 BEGIN
-	DECLARE EXIT HANDLER FOR 1452
-	SELECT 'MySQL error 1452: Cannot add or update a child row: a foreign key constraint fails';
-	UPDATE posts 
+	UPDATE posts p
 		SET description = _description
-		WHERE post_id = _post_id AND posts.is_active = 1;
+		WHERE post_id = _post_id
+			AND p.is_active = 1
+			AND p.user_id = 1;
 END;
 //
 DELIMITER ;
 
 
-/* This stored procedure when executed, deactivates the post */
-/* What to do if any one or all of these: user_id or post_id is null  */
+/* Deactivates the post, by setting the is_active value to zero */
+/* QUESTION: What to do if any one (or) all of the input parameters are null  */
 DROP PROCEDURE IF EXISTS delete_post;
 DELIMITER //
 CREATE PROCEDURE delete_post
@@ -150,20 +162,20 @@ CREATE PROCEDURE delete_post
 	IN _post_id INTEGER
 )
 BEGIN
-	DECLARE EXIT HANDLER FOR 1452
-	SELECT 'MySQL error 1452: Cannot add or update a child row: a foreign key constraint fails';
-	
 	UPDATE posts 
-	    SET is_active = 0
-		WHERE post_id = _post_id AND posts.is_active = 1;
+		SET is_active = 0
+		WHERE post_id = _post_id 
+			AND posts.is_active = 1;
 	
 	UPDATE votes
 		SET is_active = 0
-		WHERE votes.post_id = _post_id AND votes.is_active = 1;
+		WHERE votes.post_id = _post_id 
+			AND votes.is_active = 1;
 	
 	UPDATE comments
 		SET is_active = 0
-		WHERE comments.post_id = _post_id AND comments.is_active = 1;
+		WHERE comments.post_id = _post_id 
+			AND comments.is_active = 1;
 END;
 //
 DELIMITER ;
