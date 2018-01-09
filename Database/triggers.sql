15cda1e933325e7bb5960a6e8239d33b24a43ba7
manojkrajasekar <manoj.k.rajasekar@gmail.com>
2018-01-09 14:33:00 -0500

    Queries for tiriggers

---
+++ Database/triggers.sql
@@ -0,0 +1,16 @@
+use photoapp;
+
+/* When a user posts a pic, this updates the number of posts for that user */
+DROP TRIGGER IF EXISTS update_post_count;
+DELIMITER //
+CREATE TRIGGER update_post_count
+    AFTER INSERT 
+    ON posts
+    FOR EACH ROW 
+BEGIN
+    UPDATE users u
+	    SET u.no_of_posts = u.no_of_posts + 1 
+        WHERE u.user_id = NEW.user_id; 
+END ;
+//
+DELIMITER ;
